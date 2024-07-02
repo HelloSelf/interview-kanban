175b1f2b6f4cb8be9fcb1d4f15379826fef78cb1
Eva Fung <fungyingwa@gmail.com>
2024-07-02 18:06:06 +0100

    Tasks completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+type Inputs = { title: string, name: string, age: string, email: string, phone: string }
+type Errors = Partial<Record<keyof Inputs, string>>
+
 function App() {
+  const [inputs, setInputs] = useState<Inputs>({ title: "", name: "", age: "", email: "", phone: "" });
+  const [errors, setErrors] = useState<Errors>({});
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [unclaimedList, setUnclaimedList] = useState<Array<Inputs>>(JSON.parse(localStorage.getItem("unclaimedList") || '""'));
+  const firstContactList = [];
+  const workOfferList = [];
+  const therapistsList = [];
+
+  const validate = (formInputs: Inputs): Errors => {
+    setIsUpdated(true);
+    const newErrors: Errors = {};
+
+    if (formInputs.title.trim() === "") {
+      newErrors.title = "Title cannot be empty."
+    }
+    var specialChars = /[^a-zA-Z ]/g;
+    if (formInputs.name.match(specialChars)) {
+      newErrors.name = "Please enter a valid name."
+    }
+    if (isNaN(Number(formInputs.age))) {
+      newErrors.age = "Please enter a valid number."
+    }
+    if (!formInputs.email.includes("@")) {
+        newErrors.email = "Please enter a valid email."
+    }
+    if (formInputs.phone.trim() === "") {
+      newErrors.phone = "Phone cannot be empty."
+    }
+
+    return newErrors;
+}
+
+  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formElements = form.elements as typeof form.elements & {
+      title: {value: string},
+      name: {value: string},
+      age: {value: string},
+      email: {value: string},
+      phone: {value: string}
+    }
+    if(!(errors.title || errors.name || errors.age || errors.email || errors.phone) && isUpdated) {
+      let inputs: Inputs = {
+        title: formElements.title.value,
+        name: formElements.name.value,
+        age: formElements.age.value,
+        email: formElements.email.value,
+        phone: formElements.phone.value,
+      };
+      localStorage.setItem("unclaimedList", JSON.stringify([...unclaimedList, inputs]));
+      setUnclaimedList([...unclaimedList, inputs]);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,28 +76,87 @@ function App() {
           }}
         >
           <b>Form</b>
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input type="text" id="title" name="title" />
-          </div>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
-          </div>
-          <div>
-            <label htmlFor="age">Age:</label>
-            <input type="text" id="age" name="age" />
-          </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input type="text" id="email" name="email" />
-          </div>
-          <div>
-            <label htmlFor="phone">Phone:</label>
-            <input type="text" id="phone" name="phone" />
-          </div>
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={onSubmit}>
+            <div className="mb-4">
+              <div>
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">Title:</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  id="title"
+                  name="title"
+                  onChange={event => {
+                    setInputs({ ...inputs, title: event.target.value });
+                    setErrors(validate({ ...inputs, title: event.target.value }));
+                  }}
+                  value={inputs.title}
+                />
+                {errors.title? <p className="text-red-500 text-xs italic">{errors.title}</p> : null}
+              </div>
+              <div>
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Name:</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  id="name"
+                  name="name"
+                  onChange={event => {
+                    setInputs({ ...inputs, name: event.target.value })
+                    setErrors(validate({ ...inputs, name: event.target.value }));
+                  }}
+                  value={inputs.name}
+                />
+                {errors.name? <p className="text-red-500 text-xs italic">{errors.name}</p> : null}
+              </div>
+              <div>
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="age">Age:</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  id="age"
+                  name="age"
+                  onChange={event => {
+                    setInputs({ ...inputs, age: event.target.value })
+                    setErrors(validate({ ...inputs, age: event.target.value }));
+                  }}
+                  value={inputs.age}
+                />
+                {errors.age? <p className="text-red-500 text-xs italic">{errors.age}</p> : null}
+              </div>
+              <div>
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email:</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  id="email"
+                  name="email"
+                  onChange={event => {
+                    setInputs({ ...inputs, email: event.target.value });
+                    setErrors(validate({ ...inputs, email: event.target.value }));
+                  }}
+                  value={inputs.email}
+                />                
+                {errors.email? <p className="text-red-500 text-xs italic">{errors.email}</p> : null}
+              </div>
+              <div>
+                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">Phone:</label>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  type="text"
+                  id="phone"
+                  name="phone"
+                  onChange={event => {
+                    setInputs({ ...inputs, phone: event.target.value })
+                    setErrors(validate({ ...inputs, phone: event.target.value }));
+                  }}
+                  value={inputs.phone}
+                />
+                {errors.phone? <p className="text-red-500 text-xs italic">{errors.phone}</p> : null}
+              </div>
+            </div>
 
-          <button>Submit</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline bg-[#bed0de]" type="submit">Submit</button>
+          </form>
         </div>
 
         <div
@@ -59,61 +176,44 @@ function App() {
               gap: "10px",
             }}
           >
-            <div
-              style={{
-                flexBasis: "100%",
-              }}
-            >
-              <b>Unclaimed</b>
-              <div
-                style={{
-                  background: "blue",
-                  border: "1px solid white",
-                  height: "100%",
-                }}
-              ></div>
+            <div className="basis-full rounded shadow-sm border-[#bed0de] border-2">
+              <div className="flex flex-row justify-between mt-2">
+                <b className="text-left w-48 h-4 mr-3 ml-3">Unclaimed</b>
+                <div className="grid justify-items-center bg-gray-300 rounded-full w-4 h-4 mr-3"><p className="text-xs mb-3 w-4 h-4 mr-3">{unclaimedList.length}</p></div>
+              </div>
+              <div className="grid grid-rows-2 gap-2 p-2">
+                {unclaimedList.map((unclaimed) => (
+                  <div className="p-2 rounded shadow-sm bg-white">
+                    <div className="flex flex-row justify-between mt-2">
+                      <p className="text-sm text-left mb-3 w-40 h-4 mr-3 font-extrabold">{unclaimed.title} {unclaimed.name}</p>
+                      <p className="text-xs mb-3 w-4 h-4 mr-3 font-light">{`${unclaimed.age}.yo`}</p>
+                    </div>
+                    <p className="text-xs mb-3 w-4 h-4 mr-3">{unclaimed.email}</p>
+                    <p className="text-xs mb-3 w-4 h-4 mr-3 font-light">{unclaimed.phone}</p>
+                  </div>
+                ))}
+              </div>
             </div>
-            <div
-              style={{
-                flexBasis: "100%",
-              }}
-            >
-              <b>First Contact</b>
-              <div
-                style={{
-                  background: "blue",
-                  border: "1px solid white",
-                  height: "100%",
-                }}
-              ></div>
+            <div className="basis-full rounded shadow-sm bg-[#bed0de]">
+              <div className="flex flex-row justify-between mt-2">
+                <b className="text-left w-48 h-4 mr-3 ml-3">First Contact</b>
+                <div className="grid justify-items-center bg-gray-300 rounded-full w-4 h-4 mr-3"><p className="text-xs mb-3 w-4 h-4 mr-3">{firstContactList.length}</p></div>
+              </div>
+              <div className="grid grid-rows-2 gap-2 p-2"></div>
             </div>
-            <div
-              style={{
-                flexBasis: "100%",
-              }}
-            >
-              <b>Preparing Work Offer</b>
-              <div
-                style={{
-                  background: "blue",
-                  border: "1px solid white",
-                  height: "100%",
-                }}
-              ></div>
+            <div className="basis-full rounded shadow-sm bg-[#bed0de]">
+              <div className="flex flex-row justify-between mt-2">
+                <b className="text-left w-48 h-4 mr-3 ml-3">Preparing Work Offer</b>
+                <div className="grid justify-items-center bg-gray-300 rounded-full w-4 h-4 mr-3"><p className="text-xs mb-3 w-4 h-4 mr-3">{workOfferList.length}</p></div>
+              </div>
+              <div className="grid grid-rows-2 gap-2 p-2"></div>
             </div>
-            <div
-              style={{
-                flexBasis: "100%",
-              }}
-            >
-              <b>Send to Therapists</b>
-              <div
-                style={{
-                  background: "blue",
-                  border: "1px solid white",
-                  height: "100%",
-                }}
-              ></div>
+            <div className="basis-full rounded shadow-sm bg-[#bed0de]">
+              <div className="flex flex-row justify-between mt-2">
+                <b className="text-left w-48 h-4 mr-3 ml-3">Send to Therapists</b>
+                <div className="grid justify-items-center bg-gray-300 rounded-full w-4 h-4 mr-3"><p className="text-xs mb-3 w-4 h-4 mr-3">{therapistsList.length}</p></div>
+              </div>
+              <div className="grid grid-rows-2 gap-2 p-2"></div>
             </div>
           </div>
         </div>
